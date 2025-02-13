d6231ac58735eb1e0cc6be0d717b373cb810ed05
Oleh Kuzhylnyi <kuzhylol@gmail.com>
2025-02-13 23:15:05 +0100

    start wifi-connect on wlan0 down and stop on wlan0 pre-up

--- recipes-connectivity/wifi-connect/files/start-wifi-connect.sh
+++ recipes-connectivity/wifi-connect/files/start-wifi-connect.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -x
 
-IFACE=$1   # Interface name (e.g., wlan0)
-EVENT=$2   # Event type (e.g., up, down, pre-up, etc.)
+sleep 15
 
 mac_address=$(cat /sys/class/net/wlan0/address | tr -d ':' | cut -c 1-8)
 
