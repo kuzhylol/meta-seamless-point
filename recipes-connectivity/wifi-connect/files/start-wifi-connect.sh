479105170683c2b0e251a7efc67d56eb341b47fa
Oleh Kuzhylnyi <kuzhylol@gmail.com>
2025-02-13 22:41:26 +0100

    detach from having internet

--- recipes-connectivity/wifi-connect/files/start-wifi-connect.sh
+++ recipes-connectivity/wifi-connect/files/start-wifi-connect.sh
@@ -5,11 +5,4 @@ EVENT=$2   # Event type (e.g., up, down, pre-up, etc.)
 
 mac_address=$(cat /sys/class/net/wlan0/address | tr -d ':' | cut -c 1-8)
 
-wget --spider http://google.com 2>&1
-
-if [ $? -eq 0 ]; then
-    printf 'Skipping WiFi Connect\n'
-else
-    printf 'Starting WiFi Connect\n'
-    wifi-connect -u /usr/share/wifi-connect/ui --portal-ssid "SeamlessPoint-${mac_address}"
-fi
+wifi-connect -u /usr/share/wifi-connect/ui --portal-ssid "SeamlessPoint-${mac_address}"
