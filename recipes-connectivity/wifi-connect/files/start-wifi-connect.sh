808e1a3ab35539ef423b38d110de83643b50aacc
Oleh Kuzhylnyi <kuzhylol@gmail.com>
2025-04-16 23:30:29 +0200

    fix start-wifi-connect.sh by restarting wifi-connect.service on wifi down

--- recipes-connectivity/wifi-connect/files/start-wifi-connect.sh
+++ recipes-connectivity/wifi-connect/files/start-wifi-connect.sh
@@ -2,12 +2,12 @@
 
 start() {
     mac_address=$(cat /sys/class/net/wlan0/address | tr -d ':' | cut -c 1-8)
-    wifi-connect -u /usr/share/ui/ --portal-ssid "SeamlessPoint-${mac_address}"
+    wifi-connect -u /usr/share/ui/build --portal-ssid "SeamlessPoint-${mac_address}"
 }
 
 if [ "$1" = "wlan0" ] && [ "$2" = "down" ]; then
     echo "Starting wifi-connect..."
-    start
+    systemctl start wifi-connect.service
     exit 0
 fi
 
