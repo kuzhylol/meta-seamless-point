ec4f3729d8cc520279c39042deaca193228561a0
Oleh Kuzhylnyi <kuzhylol@gmail.com>
2025-04-20 18:44:06 +0200

    SP-15: Fix starting wifi-connect on boot
    The wifi-connect starts even if WiFi networking exists so device doesn't connect to wifi.
    Add try counter to start wifi-connect only if WiFi connection
    disappeared.

--- recipes-connectivity/wifi-connect/files/start-wifi-connect.sh
+++ recipes-connectivity/wifi-connect/files/start-wifi-connect.sh
@@ -1,17 +1,30 @@
 #!/bin/sh -x
 
+MAX_RETRIES=30
+SLEEP_TIME=1
+RETRY=0
+
 start() {
+    echo "Starting wifi-connect..."
     mac_address=$(cat /sys/class/net/wlan0/address | tr -d ':' | cut -c 1-8)
     wifi-connect -u /usr/share/ui/build --portal-ssid "SeamlessPoint-${mac_address}"
 }
 
-if [ "$1" = "wlan0" ] && [ "$2" = "down" ]; then
-    echo "Starting wifi-connect..."
-    systemctl start wifi-connect.service
-    exit 0
-fi
-
 if [ "$1" = "start" ]; then
-    ip route | grep -q "default" || start
+    while [ $RETRY -lt $MAX_RETRIES ]; do
+        if ip route | grep -q "default"; then
+            echo "Default route exists, skipping start"
+            exit 0
+        else
+            echo "No default route found. Attempt #$((RETRY + 1))"
+            sleep $SLEEP_TIME
+            RETRY=$((RETRY + 1))
+        fi
+    done
+
+    start
 fi
 
+if [ "$1" = "wlan0" ] && [ "$2" = "down" ]; then
+    systemctl start wifi-connect.service
+fi
